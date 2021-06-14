42520f0e9c96e6663888a4ac71faab5e886e0491
Cyprian Nyakundi <cyprian@Cyprians-MacBook-Pro.local>
2021-06-14 13:09:08 +0300

    second commit

--- gscrollnews/settings.py
+++ gscrollnews/settings.py
@@ -22,9 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z3k(m61=w2jp9347ekh+u41z2-fa6%i-b88l&8cb0r@^_*93o#'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
